ae7481bb1602c57ead2d3129c8edcd4271c623ba
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-11-27 08:49:36 +0100

    update ButtonAccent

--- docs/src/components/ButtonAccent.svelte
+++ docs/src/components/ButtonAccent.svelte
@@ -8,6 +8,6 @@ interface Props {
 let { text }: Props = $props();
 </script>
 
-<button class="cursor-pointer bg-[#5b2ff5] text-white px-5 py-2.5 rounded transition-transform duration-200 border-none !mt-0 active:scale-105">
+<button class="font-semibold cursor-pointer bg-[#5b2ff5] hover:bg-[#4c1fd4] text-white px-5 py-2.5 rounded transition-transform duration-200 border-none !mt-0 active:scale-105">
 {text}
 </button>
