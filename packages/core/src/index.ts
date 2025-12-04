2020ee9dc02c97e76c36d9def7844905fcd5cfdf
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-12-04 14:33:58 +0100

    export new modules

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,3 +2,5 @@ export * from './NumericInput';
 export * from './utils/event-handlers';
 export * from './utils/sanitization';
 export * from './utils/decimals';
+export * from './utils/scientific-notation';
+export * from './utils/formatting';
