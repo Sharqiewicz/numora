1ab0338dc34d124b1b54420c5f9a305b2b11d04d
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-12-13 01:35:51 +0100

    remove old astro docs

--- docsv0/src/components/GithubButton.svelte
+++
@@ -1,1 +0,0 @@
-<iframe src="https://ghbtns.com/github-btn.html?user=sharqiewicz&repo=numora&type=star&count=true&size=large" frameborder="0" scrolling="0" width="170" height="30" title="GitHub"></iframe>
