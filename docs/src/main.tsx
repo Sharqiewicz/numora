7bdd9f4ddfb2bc3557ef32d9c4975e2339d0306e
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-12-15 11:29:41 +0100

    disable ssr for docs

---
+++ docs/src/main.tsx
@@ -0,0 +1,15 @@
+import { createRoot } from 'react-dom/client'
+import { RouterProvider } from '@tanstack/react-router'
+
+import { getRouter } from './router'
+import './styles.css'
+
+const router = getRouter()
+
+const rootElement = document.getElementById('root')
+if (!rootElement) {
+  throw new Error('Root element "#root" not found')
+}
+
+createRoot(rootElement).render(<RouterProvider router={router} />)
+
