530a9192aa01c72715a1cec983558fc9ebace796
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-12-15 11:12:17 +0100

    disable ssr for docs

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -4,20 +4,19 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-import netlify from '@netlify/vite-plugin-tanstack-start'
 
 const config = defineConfig({
-  ssr: {
-    noExternal: ['react-syntax-highlighter'],
-  },
   plugins: [
     devtools(),
-    netlify(),
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
-    tanstackStart(),
+    tanstackStart({
+      spa: {
+        enabled: true,
+      },
+    }),
     viteReact(),
   ],
 
