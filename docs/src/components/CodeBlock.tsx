3cd3196fcb255fbecdee97cd734e3503e259c340
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-12-15 10:24:21 +0100

    wip

--- docs/src/components/CodeBlock.tsx
+++ docs/src/components/CodeBlock.tsx
@@ -1,5 +1,5 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import { oneDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 import { cn } from '@/lib/utils'
 
 interface CodeBlockProps {
