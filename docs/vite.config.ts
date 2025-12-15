442b3eec711c56d2c37599a1110e0e3650e94e04
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-12-15 11:27:03 +0100

    disable ssr for docs

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,25 +1,19 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import tanstackRouter from '@tanstack/router-plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-import netlify from '@netlify/vite-plugin-tanstack-start'
 
 const config = defineConfig({
   plugins: [
     devtools(),
+    tanstackRouter(),
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
-    tanstackStart({
-      spa: {
-        enabled: true,
-      },
-    }),
     viteReact(),
-    netlify(),
   ],
 
 })
